f5b06128ba26ce7f815f9af8d48432b42f58178e
Luke Schwab <schwab.luke@gmail.com>
2024-01-02 18:54:19 -0600

    added tic-tac-toe
    added tic-tac-toe embed game. fully functional. could use some visual polish.

--- LukeBot/LukeBot/cogs/TextGames.py
+++ LukeBot/LukeBot/cogs/TextGames.py
@@ -1,4 +1,5 @@
-﻿import bot
+﻿from calendar import c
+import bot
 import discord
 from discord import app_commands
 from discord.ext import commands
@@ -196,6 +197,20 @@ class TextGames(commands.Cog):
     @commands.hybrid_command(name="picdle", description="Guess the animal!")
     async def picdle(self, ctx, difficulty):
         """Guess cat or dog. Easy, Med, or Hard"""
+        # Image from URL into bytes to send in discord embed
+        def urlToImgFile(response, embed, blur = None):
+
+                image_data = BytesIO(response.content)
+                image = Image.open(image_data)
+                if blur != None:
+                    image = image.filter(ImageFilter.BoxBlur(blur))
+                image_data = BytesIO()  # Create a new BytesIO object for saving the image
+                image.save(image_data, format='PNG')
+                image_data.seek(0)
+                fileImage = discord.File(image_data, filename="image.png")
+                embed.set_image(url="attachment://image.png")
+                return fileImage
+        
         animalOptions = {0: "dog", 1: "cat"}
         difficulties = {"easy": 10, "med": 20, "hard": 40}
         update = None
@@ -219,31 +234,17 @@ class TextGames(commands.Cog):
             if response.status_code == 200:
                 animal_embed = discord.Embed(title="Cat or Dog?", description="To guess, type 'cat' or 'dog'. 'end' will end the game.")
     
-                # Image from URL into bytes to send in discord message
-                image_data = BytesIO(response.content)
-                image = Image.open(image_data)
-                image = image.filter(ImageFilter.BoxBlur(selectedDifficulty))
-                image_data = BytesIO()  # Create a new BytesIO object for saving the image
-
-                image.save(image_data, format='PNG')
-                image_data.seek(0)
-                imageToFile = discord.File(image_data, filename="image.png")
-                
-                animal_embed.set_image(url="attachment://image.png")
+                fileImage = urlToImgFile(response, animal_embed, selectedDifficulty)
                 
                 if firstSend == True:
-                    update = await ctx.send(embed=animal_embed, file=imageToFile)
+                    update = await ctx.send(embed=animal_embed, file=fileImage)
                     firstSend = False
                 else:
-                    await update.edit(embed=animal_embed, attachments=[imageToFile])
+                    await update.edit(embed=animal_embed, attachments=[fileImage])
 
                 # reload image and make it unblurred. this does not update it yet, we will that later after user input
-                image_data = BytesIO(response.content)
-                image = Image.open(image_data)
-                image_data = BytesIO()  # Create a new BytesIO object for saving the image
-                image.save(image_data, format='PNG')
-                image_data.seek(0)
-                imageToFile = discord.File(image_data, filename="image.png")
+                fileImage = urlToImgFile(response, animal_embed)
+                
             else:
                 await ctx.send("Image not fetched")
                 break
@@ -253,14 +254,14 @@ class TextGames(commands.Cog):
                 user_guess = await self.bot.wait_for("message", timeout=30.0, check = lambda mess: mess.author == ctx.author and (mess.content == 'cat' or mess.content.lower() == 'dog'or mess.content.lower() == 'end'))
             except asyncio.TimeoutError: # if time out, show unblurred image and end game
                 await ctx.send(f"Time limit for guess exceeded! Answer was: {animalOptions[coinflip]}")
-                await update.edit(embed=animal_embed, attachments=[imageToFile])
+                await update.edit(embed=animal_embed, attachments=[fileImage])
                 return
 
-            await update.edit(embed=animal_embed, attachments=[imageToFile]) # update discord embed with unblurred image
+            await update.edit(embed=animal_embed, attachments=[fileImage]) # update discord embed with unblurred image
 
             if str(user_guess.content).lower() == animalOptions[coinflip]: # user correct guess
                 await update.add_reaction("✅")
-                await asyncio.sleep(2)
+                await asyncio.sleep(1)
                 await update.remove_reaction(emoji="✅", member=self.bot.user)
             elif str(user_guess.content).lower() == 'end': # user ends game
                 await ctx.send(f"Game ended. Answer was: {animalOptions[coinflip]}")
@@ -270,5 +271,126 @@ class TextGames(commands.Cog):
                 return
 
             
+    @commands.hybrid_command(name="ttt", description="challenge another user to tic-tac-toe")
+    async def ttt(self, ctx, user: discord.Member):
+        """ Challenge another user to Tic-Tac-Toe"""
+        def addTurn(firstStart, place=None, letter=None): # creates and fills grid based on list of user placement
+            if firstStart == True:
+                grid = Image.open('assets/tictactoe/grid.png') # create image background
+            else:
+                grid = Image.open('assets/tictactoe/gridNEW.png')
+            match place:
+                case 1:
+                    grid.paste(Image.open(f'assets/tictactoe/{letter}.png'), (0,0))
+                case 2:
+                    grid.paste(Image.open(f'assets/tictactoe/{letter}.png'), (167,0))
+                case 3:
+                    grid.paste(Image.open(f'assets/tictactoe/{letter}.png'), (334,0))
+                case 4:
+                    grid.paste(Image.open(f'assets/tictactoe/{letter}.png'), (0,167))
+                case 5:
+                    grid.paste(Image.open(f'assets/tictactoe/{letter}.png'), (167,167))
+                case 6:
+                    grid.paste(Image.open(f'assets/tictactoe/{letter}.png'), (334,167))
+                case 7:
+                    grid.paste(Image.open(f'assets/tictactoe/{letter}.png'), (0,334))
+                case 8:
+                    grid.paste(Image.open(f'assets/tictactoe/{letter}.png'), (167,334))
+                case 9:
+                    grid.paste(Image.open(f'assets/tictactoe/{letter}.png'), (334,334))
+                case _:
+                    pass
+            grid.save('assets/tictactoe/gridNEW.png')
+            grid.close()
+            return discord.File("assets/tictactoe/gridNEW.png")
+        
+        # discord user accept/deny challenge
+        await ctx.send(f"{user.mention}: You have 10 seconds to accept or deny {ctx.author.mention}'s TIC-TAC-TOE challenge. 'accept' or 'deny'.")
+        try:
+            userAns = await self.bot.wait_for("message", check = lambda mess: mess.author == user and (mess.content.lower() == 'accept' or mess.content.lower() == 'deny'), timeout=10.0)
+        except asyncio.TimeoutError:
+            await ctx.send(f"Game cancelled. {user.mention} failed to respond.")
+            return
+        if userAns == 'deny':
+            await ctx.send(f"{user.mention} denied TIC-TAC-TOE.")
+            return
+        await ctx.send(f"Game starting! {ctx.author.mention} has first move.")
+        
+        userPlaced = {
+            1: None, 2: None, 3: None, 
+            4: None, 5: None, 6: None, 
+            7: None, 8: None, 9: None
+            }
+        
+        winCombinations = [
+          [1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9],
+          [1, 5, 9],
+          [3, 5, 7],
+          [1, 4, 7],
+          [2, 5, 8],
+          [3, 6, 9]
+        ]
+        # set up initial embed
+        ttt_embed = discord.Embed(title="TIC-TAC-TOE", description="Type desired grid number for your turn.")
+        emptyGrid = addTurn(True) # pass no args except bool to create new empty grid
+        ttt_embed.set_image(url='attachments://gridNEW.png')
+        update = await ctx.send(embed=ttt_embed, file=emptyGrid)
+        
+        # flag for win con
+        flag = False
+        for i in range(9):
+            userNum = None # for scope
+            # user enters 1-9 to make their turn.
+            try:
+                if i % 2 == 0:
+                    getMsg = await self.bot.wait_for("message", check = lambda mess: mess.author == ctx.author and (int(mess.content) < 10 and int(mess.content) > 0 and userPlaced[int(mess.content)] is None), timeout=15.0)
+                    userNum = int(getMsg.content)
+                    userPlaced[userNum] = 'x'
+                else:
+                    getMsg = await self.bot.wait_for("message", check = lambda mess: mess.author == user and (int(mess.content) < 10 and int(mess.content) > 0 and userPlaced[int(mess.content)] is None), timeout=15.0)
+                    userNum = int(getMsg.content)
+                    userPlaced[userNum] = 'o'
+            except asyncio.TimeoutError:
+                await ctx.send("User failed to respond in time. Game forfeited.")
+                break
+                
+            # update grid
+            gridFile = addTurn(False, userNum, userPlaced[userNum])
+            await update.edit(embed=ttt_embed, attachments=[gridFile])
+            
+            # check for win
+            if i > 3: # no need to check unless 3 x's minimum have been placed
+                for win in winCombinations:
+                    if userPlaced[win[0]] == 'x' and userPlaced[win[1]] == 'x' and userPlaced[win[2]] == 'x':
+                        await ctx.send(f"TIC-TAC-TOE!: User {ctx.author.mention} has won!")
+                        flag = True
+                        break
+                    elif userPlaced[win[0]] == 'o' and userPlaced[win[1]] == 'o' and userPlaced[win[2]] == 'o':
+                        await ctx.send(f"TIC-TAC-TOE!: User {ctx.author.mention} has won!")
+                        flag = True
+                        break
+                    
+            # if win is true break
+            if flag == True:
+                break
+        
+        # if loop has passed and no win condition has been met, no winner (cat's game)
+        if flag == False:
+            await ctx.send("Cat's game!")
+        
+        # remove generated png file
+        os.remove('assets/tictactoe/gridNEW.png')
+            
+
+            
+                
+                
+        
+        
+        
+    
+    
 async def setup(bot):
     await bot.add_cog(TextGames(bot))
